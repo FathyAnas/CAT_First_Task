098a0b8f175d966667fcce4191a041fe649f181f
Fathy Anas <fathyanas10717@gmail.com>
2024-02-18 00:14:28 +0200

    Added week 2 task - tool chain

---
+++ Week 2 - Pre processor/Task 1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include"Task 1.h"
+
+
+#ifdef X_VALUE   
+	
+	printf("%d",X_VALUE);
+	
+#else  
+
+	printf("%d",-1);
+
+#endif
---
+++ Week 2 - Pre processor/Task 1.h
@@ -0,0 +1,8 @@
+
+#ifndef TASK1_H_
+#define TASK1_H_
+
+#define X_VALUE  5 
+
+
+#endif
